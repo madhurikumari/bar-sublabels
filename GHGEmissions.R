bd370a164dcdadd02279246cb3adf75011829f8a
madhuri-kumari <47047221+madhurikumari@users.noreply.github.com>
2019-03-26 01:41:36 -0400

    Update GHGEmissions.R

--- GHGEmissions.R
+++ GHGEmissions.R
@@ -41,18 +41,26 @@ ggplot( aes(x = Melt ,y = df_1clean$X.ktCO2e. / 1000, fill=Color)) +
            size = 5, angle=90, hjust=1) +
   annotate(geom = "text", x = c(2,4.5,7,9.5,12,14.5), y = -75, label = c("BAU", "ZEV","BAU", "ZEV","BAU", "ZEV"), size = 5) +
   annotate(geom = "text", x = c(3,8,13), y = -105, label = c("LDVs", "Trucks", "Combined"), size = 5) +
-  annotate("segment", x = 0.5, xend = 0.5, y = -115, yend = 0,size=1)+
+  annotate("segment", x = 0.5, xend = 0.5, y = -115, yend = 225,size=1)+
   annotate("segment", x = 3.5, xend = 3.5, y = -90, yend = 0)+
-  annotate("segment", x = 5.5, xend = 5.5, y = -115, yend = 0,size=1)+
+  annotate("segment", x = 5.5, xend = 5.5, y = -115, yend = 225,size=1)+
   annotate("segment", x =8.5, xend = 8.5, y = -90, yend = 0)+
-  annotate("segment", x = 10.5, xend = 10.5, y = -115, yend = 0,size=1)+
+  annotate("segment", x = 10.5, xend = 10.5, y = -115, yend = 225,size=1)+
   annotate("segment", x =13.5, xend = 13.5, y = -90, yend = 0)+
-  annotate("segment", x = 15.5, xend = 15.5, y = -115, yend = 0,size=1)+
+  annotate("segment", x = 15.5, xend = 15.5, y = -115, yend = 225,size=1)+
   annotate("segment", x = 0.5, xend = 15.5, y = -115, yend = -115)+
   annotate("segment", x = 0.5, xend = 15.5, y = -90, yend = -90)+
-  annotate(geom = "text", x = seq_len(nrow(df))-0.1, y = (df_1clean$X.ktCO2e./1000) + 10, 
+  
+  annotate("segment", x = 0.6, xend = 5.5, y = df_1clean$X.ktCO2e.[1]/1000, yend = df_1clean$X.ktCO2e.[1]/1000, 
+           linetype = 2, color="grey50")+
+  annotate("segment", x = 5.6, xend = 10.5, y = df_1clean$X.ktCO2e.[6]/1000, yend = df_1clean$X.ktCO2e.[6]/1000,
+           linetype = 2, color="grey50")+
+  annotate("segment", x = 10.6, xend = 15.5, y = df_1clean$X.ktCO2e.[11]/1000, yend = df_1clean$X.ktCO2e.[11]/1000,
+           linetype = 2, color="grey50")+
+  
+  annotate(geom = "text", x = seq_len(nrow(df))-0.1, y = (df_1clean$X.ktCO2e./1000) + 15, 
            label = as.integer(df$Change.in.Emission*100), size = 5) +
-  annotate(geom = "text", x = seq_len(nrow(df))+0.3,y = (df_1clean$X.ktCO2e./1000) + 10, 
+  annotate(geom = "text", x = seq_len(nrow(df))+0.3,y = (df_1clean$X.ktCO2e./1000) + 15, 
            label = rep(c("","%","%","%","%"), times=3) , size = 5) +
   labs(title = "GHG Emissions", y="Emissions (mt CO2e)") + 
   coord_cartesian(ylim = c(0, 225), expand = FALSE, clip = "off") +
